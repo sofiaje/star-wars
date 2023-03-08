dfba0ab3f62044cacb45b6a108ac5a19d972ee7d
Sofia <sofia.jespersen@hotmail.com>
2023-03-08 17:15:35 +0100

    get data, create instances

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+//global
+let charactersDiv = document.getElementById("charactersDiv");
+const getDataBtn = document.getElementById("getDataBtn")
+
+const list1 = document.getElementById("list1");
+const list2 = document.getElementById("list2");
+
 
 //class character
 class Character {
@@ -5,9 +12,9 @@ class Character {
         this.name = name;
         this.height = height;
         this.mass = mass;
-        this.hairColor = hairColor;
-        this.skinColor = skinColor;
-        this.eyeColor = eyeColor;
+        this.hair_color = hairColor;
+        this.skin_color = skinColor;
+        this.eye_color = eyeColor;
         this.gender = gender;
         this.films = films;
         // this.img = img;
@@ -20,25 +27,57 @@ async function getData(url) {
     let res = await fetch(`https://swapi.dev/api/${url}`);
     let data = await res.json();
 
-    console.log(data)
     return data
 }
 
 
-let luke = new Character("Luke skywalker", 172, 77, "blond", "fair", "blue", "male", 5);
-
-// console.log(luke)
 
 
-
-
-async function loadPage() {
-    let person = await getData("people/4");
+async function createInstance(value) {
+    let person = await getData(`people/${value}`);
 
     let { name, height, mass, hair_color, skin_color, eye_color, gender, films } = person
-    
+
     let newPerson = new Character(name, height, mass, hair_color, skin_color, eye_color, gender, films)
-    console.log(newPerson)
+    // console.log(newPerson)
+    return newPerson
 }
 
-loadPage()
+// loadPage()
+
+
+
+
+
+getDataBtn.addEventListener("click", async function (e) {
+    e.preventDefault();
+
+    if (parseInt(list1.value) !== 0 && parseInt(list2.value) !== 0) {
+        let firstCharacter = await createInstance(list1.value);
+        let secondCharacter = await createInstance(list2.value);
+    
+        charactersDiv.innerHTML = "";
+        displayData(firstCharacter);
+        displayData(secondCharacter);
+    } else {
+        console.log("du måste välja karaktärer")
+    }
+})
+
+
+
+function displayData(character) {
+    // console.log(character)
+    let { name, height, mass, hair_color, skin_color, eye_color, gender, films } = character
+
+    let section = document.createElement("section")
+    section.innerHTML = `<h2>${name}</h2>
+    <p>Height: ${height}<br>
+    Mass: ${mass}<br>
+    Hair: ${hair_color}<br>
+    Skin color: ${skin_color}<br>
+    Eye color: ${eye_color}<br>
+    Gender: ${gender}<br>
+    Films: ${films.length}<br></p>`
+    charactersDiv.append(section);
+}
